b2be8fd049303758fb697602e2dee9b3ccecb8ef
Ridowan-sajid <sajidridowan7@gmail.com>
2023-09-22 19:59:05 +0600

    Seeding data Added

--- NZWalks.API/Migrations/20230922135330_Seed added.Designer.cs
+++ NZWalks.API/Migrations/20230922135330_Seed added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.API;
 
@@ -11,9 +12,11 @@ using NZWalks.API;
 namespace NZWalks.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230922135330_Seed added")]
+    partial class Seedadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- NZWalks.API/Models/Domain/Region.cs
+++ NZWalks.API/Models/Domain/Region.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
-        public string RegionImgUrl { get; set; }
+        public string? RegionImgUrl { get; set; }
     }
 }
